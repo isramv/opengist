0e0d56b54f29ddc1429dd1b8546d429dabf8f0d6
Israel Morales <israel.morales@rocket.com.mx>
2016-01-19 13:06:55 -0800

    New expense form

--- src/Rocket/ExpensesBundle/Controller/DefaultController.php
+++ src/Rocket/ExpensesBundle/Controller/DefaultController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Rocket\ExpensesBundle\Entity\Expense;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller {
     public function indexAction() {
@@ -18,29 +20,23 @@ class DefaultController extends Controller {
     }
     public function createItemAction(Request $request)
     {
-        $date = new \DateTime('now');
-        $expense = new Expense();
-        $expense->setAmount(100.00);
-        $expense->setDate($date);
-        $expense->setDescription('Test Expense');
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($expense);
-        // $em->flush();
-//        $html = $this->container->get('templating')->render('RocketExpensesBundle:Default:index_expenses.html.twig');
-        // Form.
         $expense_obj = new Expense();
         $form = $this->createFormBuilder($expense_obj)
           ->add('amount', NumberType::class)
-          ->add('date', DateType::class)
+          ->add('date', DateType::class, array(
+              'input' => 'datetime',
+              'widget' => 'text',
+            ))
           ->add('description', TextType::class)
           ->add('submit', SubmitType::class)
           ->getForm();
-
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-          dump($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($form->getViewData());
+          $em->flush();
         }
 
 
