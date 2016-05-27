98cd3f2ec3a3be6fb1c92a4b9955a9f0fe67bcfe
Israel Morales <isramv@gmail.com>
2016-05-27 22:08:31 +0000

    Tags for gists

---
+++ src/BetterGistsBundle/Entity/Tags.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BetterGistsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tags
+ *
+ * @ORM\Table(name="tags")
+ * @ORM\Entity(repositoryClass="BetterGistsBundle\Repository\TagsRepository")
+ */
+class Tags
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Tags
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
