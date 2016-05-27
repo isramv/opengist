1913cd9dfffd49bf47b67c00d81a8492ad418f8b
Israel Morales <isramv@gmail.com>
2016-05-27 00:08:48 +0000

    Better gists bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Rocket\UserBundle\RocketUserBundle(),
             new Rocket\AjaxBundle\RocketAjaxBundle(),
+            new BetterGistsBundle\BetterGistsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/BetterGistsBundle/BetterGistsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BetterGistsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BetterGistsBundle extends Bundle
+{
+}
