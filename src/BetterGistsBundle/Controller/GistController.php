db5088f9c435fdad3813915e1ecf4efbea47c919
Israel Morales <israel@chapterthree.com>
2017-02-03 09:26:01 -0800

    Refactored twig to live inside the bundle

--- src/BetterGistsBundle/Controller/GistController.php
+++ src/BetterGistsBundle/Controller/GistController.php
@@ -36,279 +36,298 @@ use Symfony\Component\Yaml\Yaml;
  *
  * @Route("/gist")
  */
-class GistController extends Controller
-{
-    /**
-     * Lists all Gist entities.
-     *
-     * @Route("/", name="gist_index")
-     * @Method("GET")
-     */
-    public function indexAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $gist_repository = $em->getRepository('BetterGistsBundle:Gist');
+class GistController extends Controller {
+  /**
+   * Lists all Gist entities.
+   *
+   * @Route("/", name="gist_index")
+   * @Method("GET")
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function indexAction(Request $request) {
+    $em = $this->getDoctrine()->getManager();
+    $gist_repository = $em->getRepository('BetterGistsBundle:Gist');
 
-        // page number validation param.
+    // page number validation param.
 
-        if(!is_null($request->query->get('page'))) {
-          if (!preg_match('/^[0-9]+$/', $request->query->get('page'))) {
-                $response = new Response();
-                $response->setStatusCode(400);
-                $response->setContent('Argument needs to be integer.');
-                return $response;
-            }
-        }
-        if($request->query->get('page')) {
-            $number_of_page_requested = intval($request->query->get('page'));
-        } else {
-            $number_of_page_requested = 1;
-        }
-
-        $user_id = $this->get('security.token_storage')->getToken();
-        $user = $user_id->getUser();
-        $uid = $user->getId();
-
-        $pager = new BPaginator($gist_repository, $uid);
-        $query_params_from_request = $request->query->all();
-
-        $order_by = array();
-
-        // Now accepts query parameters like updated, created, and title for orderBy.
-
-        if(isset($query_params_from_request['updated'])) {
-          if ($query_params_from_request['updated'] === 'ASC') {
-            $order_by = array('updated', 'ASC');
-          } else if ($query_params_from_request['updated'] === 'DESC') {
-            $order_by = array('updated', 'DESC');
-          }
-        }
-
-        if(isset($query_params_from_request['created'])) {
-          if ($query_params_from_request['created'] === 'ASC') {
-            $order_by = array('created', 'ASC');
-          } else if ($query_params_from_request['created'] === 'DESC') {
-            $order_by = array('created', 'DESC');
-          }
-        }
-
-        if(isset($query_params_from_request['title'])) {
-          if ($query_params_from_request['title'] === 'ASC') {
-            $order_by = array('title', 'ASC');
-          } else if ($query_params_from_request['title'] === 'DESC') {
-            $order_by = array('title', 'DESC');
-          }
-        }
-
-        if(count($order_by) !== 0) {
-          $pager->setOrderBy($order_by);
-        }
-
-        $pager->setLimit(15);
-
-        $gists = $pager->getPage($number_of_page_requested);
-
-        foreach ($gists['items'] as $key => $gist) {
-
-          // populate tags of each result.
-          $each = $gist_repository->find($gist['id']);
-          $gists['items'][$key]['tags'] = $each->getTags()->getValues();
-          $gists['items'][$key]['getUpdatedString'] = $each->getUpdatedString();
-
-        }
-
-        return $this->render('gist/index.html.twig', array(
-            'gists' => $gists
-        ));
+    if (!is_null($request->query->get('page'))) {
+      if (!preg_match('/^[0-9]+$/', $request->query->get('page'))) {
+        $response = new Response();
+        $response->setStatusCode(400);
+        $response->setContent('Argument needs to be integer.');
+        return $response;
+      }
+    }
+    if ($request->query->get('page')) {
+      $number_of_page_requested = intval($request->query->get('page'));
+    }
+    else {
+      $number_of_page_requested = 1;
     }
 
-    /**
-     * Creates a new Gist entity test.
-     *
-     * @Route("/new", name="gist_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $tag = new Tags();
-        $gist = new Gist();
-        // $gist->getTags()->add($tag);
-        $form = $this->createForm('BetterGistsBundle\Form\GistType', $gist);
-        $form->handleRequest($request);
+    $user_id = $this->get('security.token_storage')->getToken();
+    $user = $user_id->getUser();
+    $uid = $user->getId();
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            foreach($gist->getTags() as $key=>$tag) {
-                $valueToSearch = $tag->getName();
+    $pager = new BPaginator($gist_repository, $uid);
+    $query_params_from_request = $request->query->all();
 
-                // TODO: Tags should have an author id.
-                // so users don't mess with other users tags.
-                // or remove edit of tags for the users.
+    $order_by = array();
 
-                $result = $em->getRepository('BetterGistsBundle:Tags')->findOneByName($valueToSearch);
-                if(empty($result)) {
-                    $em->persist($tag);
-                } elseif (!empty($result)) {
-                    $gist->getTags()->remove($key);
-                    $gist->getTags()->add($result);
-                }
-            }
-            $user = $this->get('security.token_storage')->getToken()->getUser();
-            $gist->setAuthor($user);
-            $em->persist($gist);
-            $date = new \DateTime('now');
-            $gist->setCreated($date);
-            $gist->setUpdated($date);
-            $em->flush();
-            return $this->redirectToRoute('gist_show', array('id' => $gist->getId()));
+    // Now accepts query parameters like updated, created, and title for orderBy.
+
+    if (isset($query_params_from_request['updated'])) {
+      if ($query_params_from_request['updated'] === 'ASC') {
+        $order_by = array('updated', 'ASC');
+      }
+      else {
+        if ($query_params_from_request['updated'] === 'DESC') {
+          $order_by = array('updated', 'DESC');
         }
-
-        return $this->render(':gist:new-edit.html.twig', array(
-            'gist' => $gist,
-            'form' => $form->createView(),
-        ));
+      }
     }
 
-    /**
-     * Finds and displays a Gist entity.
-     *
-     * @Route("/{id}", name="gist_show")
-     * @Method("GET")
-     */
-    public function showAction(Gist $gist)
-    {
-
-        if($this->isGranted('edit', $gist)) {
-          $parsedown = new \Parsedown();
-          $output = $parsedown->setMarkupEscaped(true)->text($gist->getBody());
-
-          $gist->setBody($output);
-          $deleteForm = $this->createDeleteForm($gist);
-
-          return $this->render('gist/show.html.twig', array(
-            'gist' => $gist,
-            'delete_form' => $deleteForm->createView(),
-          ));
+    if (isset($query_params_from_request['created'])) {
+      if ($query_params_from_request['created'] === 'ASC') {
+        $order_by = array('created', 'ASC');
+      }
+      else {
+        if ($query_params_from_request['created'] === 'DESC') {
+          $order_by = array('created', 'DESC');
         }
-        else {
-          return new AccessDeniedException();
-        }
+      }
+    }
 
+    if (isset($query_params_from_request['title'])) {
+      if ($query_params_from_request['title'] === 'ASC') {
+        $order_by = array('title', 'ASC');
+      }
+      else {
+        if ($query_params_from_request['title'] === 'DESC') {
+          $order_by = array('title', 'DESC');
+        }
+      }
+    }
+
+    if (count($order_by) !== 0) {
+      $pager->setOrderBy($order_by);
+    }
+
+    $pager->setLimit(15);
+
+    $gists = $pager->getPage($number_of_page_requested);
+
+    foreach ($gists['items'] as $key => $gist) {
+
+      // populate tags of each result.
+      $each = $gist_repository->find($gist['id']);
+      $gists['items'][$key]['tags'] = $each->getTags()->getValues();
+      $gists['items'][$key]['getUpdatedString'] = $each->getUpdatedString();
 
     }
 
-    /**
-     * Edit the gist with ReactJS.
-     *
-     * @Route("/{id}/editJS", name="gist_edit_react")
-     * @Method({"GET" , "POST"})
-     */
+    return $this->render('@BetterGists/gist/index.html.twig', array(
+      'gists' => $gists
+    ));
+  }
 
-    public function editReactAction(Request $request, Gist $gist)
-    {
-      // JWT Token to use with ReactJS.
-      $username = $this->get('security.token_storage')->getToken()->getUsername();
-      $jwt = $this->get('app.jwt_issuer');
-      $jwt->setUsername($username);
-      $jwtToken = $jwt->getJwt();
+  /**
+   * Creates a new Gist entity test.
+   *
+   * @Route("/new", name="gist_new")
+   * @Method({"GET", "POST"})
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+   */
+  public function newAction(Request $request) {
+    $gist = new Gist();
 
-      // response.
-      $response = $this->render(':gist:edit-react.html.twig',
-        array(
-          'gist' => $gist,
-          'jwt' => $jwtToken
-        )
-      );
-      return $response;
-    }
+    $form = $this->createForm('BetterGistsBundle\Form\GistType', $gist);
+    $form->handleRequest($request);
 
-    /**
-     * Displays a form to edit an existing Gist entity.
-     *
-     * @Route("/{id}/edit", name="gist_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Gist $gist)
-    {
-        $em = $this->getDoctrine()->getManager();
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      foreach ($gist->getTags() as $key => $tag) {
+        $valueToSearch = $tag->getName();
 
-        $editForm = $this->createForm('BetterGistsBundle\Form\GistType', $gist);
-        $editForm->handleRequest($request);
+        // TODO: Tags should have an author id.
+        // so users don't mess with other users tags.
+        // or remove edit of tags for the users.
 
-        if($editForm->isSubmitted() && $editForm->isValid()) {
-            foreach($gist->getTags() as $key=>$tag) {
-                $valueToSearch = $tag->getName();
-                $result = $em->getRepository('BetterGistsBundle:Tags')->findOneByName($valueToSearch);
-                if(empty($result)) {
-                    $em->persist($tag);
-                } elseif (!empty($result)) {
-                    $gist->getTags()->remove($key);
-                    $gist->getTags()->add($result);
-                }
-            }
-            $user = $this->get('security.token_storage')->getToken()->getUser();
-            $gist->setAuthor($user);
-            $em->persist($gist);
-            $gist->setUpdated(new \DateTime('now'));
-            $em->flush();
-            return $this->redirectToRoute('gist_show', array('id' => $gist->getId()));
+        $result = $em->getRepository('BetterGistsBundle:Tags')
+          ->findOneByName($valueToSearch);
+        if (empty($result)) {
+          $em->persist($tag);
         }
-
-        return $this->render(':gist:new-edit.html.twig', array(
-            'gist' => $gist,
-            'form' => $editForm->createView(),
-        ));
-    }
-
-    /**
-     * Confirms the deletion of the gist.
-     * @Route("/delete/{id}", name="gist_confirm_delete")
-     * @Method("GET")
-     */
-    public function confirmDeleteAction(Gist $gist)
-    {
-        $deleteForm = $this->createDeleteForm($gist);
-        return $this->render(':gist:confirm_deletion.html.twig',
-            array(
-              'gist' => $gist,
-              'delete_form' => $deleteForm->createView()
-              )
-          );
-    }
-
-    /**
-     * Deletes a Gist entity.
-     *
-     * @Route("/{id}", name="gist_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Gist $gist)
-    {
-        $form = $this->createDeleteForm($gist);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($gist);
-            $em->flush();
+        elseif (!empty($result)) {
+          $gist->getTags()->remove($key);
+          $gist->getTags()->add($result);
         }
-        return $this->redirectToRoute('gist_index');
+      }
+      $user = $this->get('security.token_storage')->getToken()->getUser();
+      $gist->setAuthor($user);
+      $em->persist($gist);
+      $date = new \DateTime('now');
+      $gist->setCreated($date);
+      $gist->setUpdated($date);
+      $em->flush();
+      return $this->redirectToRoute('gist_show', array('id' => $gist->getId()));
     }
 
-    /**
-     * Creates a form to delete a Gist entity.
-     *
-     * @param Gist $gist The Gist entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Gist $gist)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('gist_delete', array('id' => $gist->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
+    return $this->render('@BetterGists/gist/edit.html.twig', array(
+      'gist' => $gist,
+      'form' => $form->createView(),
+    ));
+  }
+
+  /**
+   * Finds and displays a Gist entity.
+   *
+   * @Route("/{id}", name="gist_show")
+   * @Method("GET")
+   * @param \BetterGistsBundle\Entity\Gist $gist
+   * @return \Symfony\Component\HttpFoundation\Response|\Symfony\Component\Security\Core\Exception\AccessDeniedException
+   */
+  public function showAction(Gist $gist) {
+
+    if ($this->isGranted('edit', $gist)) {
+      $parsedown = new \Parsedown();
+      $output = $parsedown->setMarkupEscaped(TRUE)->text($gist->getBody());
+
+      $gist->setBody($output);
+      $deleteForm = $this->createDeleteForm($gist);
+
+      return $this->render('@BetterGists/gist/show.html.twig', array(
+        'gist' => $gist,
+        'delete_form' => $deleteForm->createView(),
+      ));
     }
+    else {
+      return new AccessDeniedException();
+    }
+
+  }
+
+  /**
+   * Edit the gist with Vue.js
+   *
+   * @Route("/{id}/editJS", name="gist_edit_js")
+   * @Method({"GET" , "POST"})
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @param \BetterGistsBundle\Entity\Gist $gist
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+
+  public function editJsAction(Request $request, Gist $gist) {
+    // JWT Token to use with Vue.js
+    $username = $this->get('security.token_storage')->getToken()->getUsername();
+    $jwt = $this->get('app.jwt_issuer');
+    $jwt->setUsername($username);
+    $jwtToken = $jwt->getJwt();
+
+    // Response.
+    $response = $this->render('@BetterGists/gist/edit-js.html.twig',
+      array(
+        'gist' => $gist,
+        'jwt' => $jwtToken
+      )
+    );
+    return $response;
+  }
+
+  /**
+   * Displays a form to edit an existing Gist entity.
+   *
+   * @Route("/{id}/edit", name="gist_edit")
+   * @Method({"GET", "POST"})
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @param \BetterGistsBundle\Entity\Gist $gist
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+   */
+  public function editAction(Request $request, Gist $gist) {
+    $em = $this->getDoctrine()->getManager();
+
+    $editForm = $this->createForm('BetterGistsBundle\Form\GistType', $gist);
+    $editForm->handleRequest($request);
+
+    if ($editForm->isSubmitted() && $editForm->isValid()) {
+      foreach ($gist->getTags() as $key => $tag) {
+        $valueToSearch = $tag->getName();
+        $result = $em->getRepository('BetterGistsBundle:Tags')
+          ->findOneByName($valueToSearch);
+        if (empty($result)) {
+          $em->persist($tag);
+        }
+        elseif (!empty($result)) {
+          $gist->getTags()->remove($key);
+          $gist->getTags()->add($result);
+        }
+      }
+      $user = $this->get('security.token_storage')->getToken()->getUser();
+      $gist->setAuthor($user);
+      $em->persist($gist);
+      $gist->setUpdated(new \DateTime('now'));
+      $em->flush();
+      return $this->redirectToRoute('gist_show', array('id' => $gist->getId()));
+    }
+
+    return $this->render('@BetterGists/gist/edit.html.twig', array(
+      'gist' => $gist,
+      'form' => $editForm->createView(),
+    ));
+  }
+
+  /**
+   * Confirms the deletion of the gist.
+   * @Route("/delete/{id}", name="gist_confirm_delete")
+   * @Method("GET")
+   * @param \BetterGistsBundle\Entity\Gist $gist
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function confirmDeleteAction(Gist $gist) {
+
+    $deleteForm = $this->createDeleteForm($gist);
+
+    return $this->render('@BetterGists/gist/confirm_deletion.html.twig',
+      array(
+        'gist' => $gist,
+        'delete_form' => $deleteForm->createView()
+      )
+    );
+
+  }
+
+  /**
+   * Deletes a Gist entity.
+   *
+   * @Route("/{id}", name="gist_delete")
+   * @Method("DELETE")
+   */
+  public function deleteAction(Request $request, Gist $gist) {
+    $form = $this->createDeleteForm($gist);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->remove($gist);
+      $em->flush();
+    }
+    return $this->redirectToRoute('gist_index');
+  }
+
+  /**
+   * Creates a form to delete a Gist entity.
+   *
+   * @param Gist $gist The Gist entity
+   *
+   * @return \Symfony\Component\Form\Form The form
+   */
+  private function createDeleteForm(Gist $gist) {
+    return $this->createFormBuilder()
+      ->setAction($this->generateUrl('gist_delete', array('id' => $gist->getId())))
+      ->setMethod('DELETE')
+      ->getForm();
+  }
 }
 
